986727c723a6e667d5cd7c15ed4b63bf09420e5c
MahmutCanKahya <mahmutcankahya07@gmail.com>
2021-01-08 01:56:25 +0300

    Add project files.

---
+++ ECommerce.Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        UserLoginViewModel tempUser = new UserLoginViewModel() {  Email = "admin@ecommerce.com", Password = "123456" };
+      
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserLoginViewModel user)
+        {
+            if (user.Email == tempUser.Email && user.Password == tempUser.Password)
+            {
+                /*return Json(data: new { success = true, message = "Başarıyla giriş yapıldı." });*/
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                /*return Json(data: new { success = false, message = "E-mail veya şifrenizi yanlış tuşladınız." });*/
+                return View();
+            }
+        }
+
+       
+    }
+}
